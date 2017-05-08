721d5c644a88ff60bcd94d675a76bd20182b8174
zzc <jiayouzzc@126.com>
2017-05-08 15:25:00 +0800

    Change no.2

---
+++ static/js/tabImg.js
@@ -0,0 +1,19 @@
+function $(id){
+	return typeof id==='string'?document.getElementById(id):id;
+}
+window.onload = function(){
+	var titles = $('notice-tit').getElementsByTagName('a'),
+		divs   = $('notice-con').getElementsByTagName('div')
+
+	for(var i = 0;i<titles.length;i++){
+		titles[i].id = i;
+		titles[i].onmouseover = function(){
+			for(var j = 0;j<titles.length;j++){
+				titles[j].className=''
+				divs[j].style.display='none'
+			}
+			this.className = 'InActive'
+			divs[this.id].style.display='block'
+		}
+	}
+}
